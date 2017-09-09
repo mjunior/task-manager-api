235a0cb60f61b27654062cd3cdf80dcaffa64188
mjunior <mauricio@mjunior.com.br>
2017-09-09 21:48:49 +0000

    Adicionada tests e rota da listagem de tarefas

---
+++ spec/request/api/v1/tasks_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Tasks Api', type: :request do
+  before{ host! 'api.taskmanager.test'}
+
+  let!(:user) { create(:user) }
+  let!(:headers)do
+    {
+      'Accept' => 'application/vnd.taskmanager.v1',
+      'Content-Type' => Mime[:json].to_s,
+      'Authorization' => user.auth_token
+    }
+  end
+
+  describe 'GET /tasks' do
+    before do
+      create_list(:task, 5, user_id: user.id)
+      get '/tasks', params: {}, headers: headers
+    end
+    it 'Responde status :OK' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'Verifica o retorno das tarefas ' do
+      expect(json_body[:tasks].count).to eq(5)
+    end
+
+  end
+end
